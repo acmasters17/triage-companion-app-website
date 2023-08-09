4c53126b87c40e5a2bbd6aabb8b33895e25877c5
Adam Masters <74498986+acmasters17@users.noreply.github.com>
2023-08-09 12:47:02 +0100

    Bas

---
+++ digital-triage-companion-website/src/App.tsx
@@ -0,0 +1,103 @@
+import "./App.css";
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Privacy Policy - Digital Triage Companion</title>
+      </head>
+      <body>
+        <h1>Privacy Policy for Digital Triage Companion</h1>
+
+        <h2>Effective Date: 09/08/2023</h2>
+
+        <h3>1. Introduction</h3>
+
+        <p>
+          Welcome to Digital Triage Companion ("the App," "we," "our," or "us"). This Privacy Policy
+          is intended to inform you about how we collect, use, and disclose your personal
+          information when you use our mobile application.
+        </p>
+
+        <h3>2. Information We Collect</h3>
+
+        <p>
+          We may collect and process the following types of personal information for the purpose of
+          providing and improving our App's functionality:
+        </p>
+
+        <ul>
+          <li>
+            <strong>Email Address:</strong> We collect your email address when you sign up for an
+            account with us. This is used to facilitate account management, communication, and
+            account-related notifications.
+          </li>
+          <li>
+            <strong>Application Data:</strong> We may collect data related to your use of the App,
+            including but not limited to session duration, actions performed within the App, and
+            other usage data. This information helps us understand how you interact with the App and
+            improve its functionality.
+          </li>
+        </ul>
+
+        <h3>3. How We Use Your Information</h3>
+
+        <p>We use the collected information for the following purposes:</p>
+
+        <ul>
+          <li>Providing and improving the functionality of the App.</li>
+          <li>Communicating with you regarding your account, updates, and changes to the App.</li>
+          <li>
+            Analyzing user behavior to enhance the user experience and optimize the App's
+            performance.
+          </li>
+        </ul>
+
+        <h3>4. Legal Basis for Processing</h3>
+
+        <p>
+          We process your personal information based on the necessity to perform the contract
+          between you and us (i.e., providing you access to and use of the App) and our legitimate
+          interests in operating and improving the App.
+        </p>
+
+        <h3>5. Data Sharing and Disclosure</h3>
+
+        <p>We will not share your personal information with third-party service providers.</p>
+
+        <h3>6. Security</h3>
+
+        <p>
+          We take reasonable measures to protect your personal information from unauthorized access,
+          disclosure, alteration, or destruction. However, no data transmission over the internet or
+          electronic storage is completely secure, so we cannot guarantee its absolute security.
+        </p>
+
+        <h3>7. Your Choices</h3>
+
+        <p>
+          You have the right to access, correct, update, or delete your personal information. You
+          can do this by contacting us at mastersacm17@gmail.com or by using the profile interface
+          in app. You can also unsubscribe from our communications at any time by following the
+          instructions in our emails.
+        </p>
+
+        <h3>8. Changes to this Privacy Policy</h3>
+
+        <p>
+          We may update this Privacy Policy from time to time to reflect changes in our practices or
+          for other operational, legal, or regulatory reasons. We will notify you of any material
+          changes by posting the revised Privacy Policy on our website or within the App.
+        </p>
+
+        <h3>9. Contact Us</h3>
+
+        <p>
+          If you have any questions or concerns about this Privacy Policy or our data practices,
+          please contact us at mastersacm17@gmail.com.
+        </p>
+      </body>
+    </html>
+  );
+}
+export default App;
